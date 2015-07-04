d50b0cce70e51e0c5e93ebb9850e438080353ac7
strangestorage <watanabe@watanaberei-no-MacBook-Air.local>
2015-07-04 12:01:15 +0900

    task6

--- ReadableCode/README.txt
+++ ReadableCode/README.txt
@@ -5,7 +5,8 @@
 ＜サンプルレシピデータの名前＞
 ・recipe-data.txtとして保存
 
-＜任意のレシピデータを読み込む方法＞
-・EclipseのRun→Run Configurations→ArgumentタブのProgram arguments欄にファイル名を書く
-・ウィンドウ右下のRunで実行
-・複数行料理名が書いてあっても同様に実行することができます
+＜任意のレシピファイル、任意のIDを読み込む方法＞
+・EclipseのRun→Run Configurations→ArgumentタブのProgram arguments欄に「ファイル名 見たいID」を書く
+例：recipe-data.txt 2
+・ウィンドウ右下のRunで実行すると指定したIDのレシピが出てくる
+※IDを指定しなかった場合はすべてのレシピが出てきます
--- ReadableCode/src/ReadableCode.java
+++ ReadableCode/src/ReadableCode.java
@@ -6,25 +6,55 @@ import java.io.InputStreamReader;
 
 public class ReadableCode {
 	//task5
+	
+	Boolean fileNameExists = false;
+	Boolean targetNumExists = false;
+	String filename = "";
+	int targetNum = 0;
+	int recipeID = 0;
+	String line = "";
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for(String filename : args){
-			try {
-				FileInputStream inputStream = new FileInputStream(filename);
-				BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-				String line = "";
-				int recipeID = 0;
-				while((line = bufferedReader.readLine())!=null){
-					recipeID++;
-					System.out.println(recipeID + ":"+ line);
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
+		ReadableCode rc = new ReadableCode(args);
+	}
+	
+	public ReadableCode(String[] args){
+		init(args);
+		
+		try {
+			FileInputStream inputStream = new FileInputStream(filename);
+			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+			while((line = bufferedReader.readLine())!=null){
+				recipeID++;
+				printRecipe();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void init(String args[]){
+		if(args.length==0){
+			System.out.println("please input filename");
+			return;
+		}else{
+			filename = args[0];
+			fileNameExists = true;
+			if(args.length==2){
+				targetNum = Integer.parseInt(args[1]);
+				targetNumExists = true;
 			}
 		}
-		
-
-
+	}
+	
+	public void printRecipe(){
+		if(!targetNumExists){
+			System.out.println(recipeID + ":"+ line);
+		}
+		else{
+			if(recipeID==targetNum)System.out.println(recipeID + ":"+ line);
+		}
 	}
 
 }
